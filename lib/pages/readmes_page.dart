c8a36ef9d7393d2e178d092e659616b287bb4e92
David MacCallum <david.maccallum@takealot.com>
2021-07-09 17:41:40 +0200

    Image caching.

--- lib/pages/readmes_page.dart
+++ lib/pages/readmes_page.dart
@@ -4,7 +4,9 @@ import 'package:align/services/microservice_service.dart';
 import 'package:align/services/settings_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ReadmesPage extends StatefulWidget {
   ReadmesPage({Key? key}) : super(key: key);
@@ -41,62 +43,18 @@ class _ReadmesPageState extends State<ReadmesPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Readme"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {
-              load();
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsPage()),
-              );
-            },
-          ),
-        ],
+        actions: buildActions(context),
       ),
       body: _microservices != null
           ? Container(
               child: Row(
                 children: [
-                  Container(
-                    width: 300,
-                    child: ListView(
-                      children: getMicroserviceListTiles(),
-                    ),
-                  ),
+                  buildNav(context),
                   Expanded(
                     child: Column(
                       children: [
-                        Container(
-                          child: Row(
-                            children: [
-                              _selectedMicroserice != null
-                                  ? TextButton(
-                                      onPressed: () {
-                                        launch(_selectedMicroserice!.repo.url);
-                                      },
-                                      child:
-                                          Text(_selectedMicroserice!.repo.url),
-                                    )
-                                  : Container(),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Markdown(
-                            // data: _dummyReadme
-                            data: _selectedMicroserice != null
-                                ? _selectedMicroserice!.readme.readme
-                                    .replaceAll(
-                                        RegExp(r'\|\s+$', multiLine: true), "|")
-                                : "",
-                          ),
-                        ),
+                        buildContentActionBar(),
+                        buildContent(),
                       ],
                     ),
                   ),
@@ -109,14 +67,88 @@ class _ReadmesPageState extends State<ReadmesPage> {
     );
   }
 
-  List<Widget> getMicroserviceListTiles() {
-    return _microservices != null
-        ? _microservices!
+  buildContent() {
+    return Expanded(
+      child: Markdown(
+        data: _selectedMicroserice != null
+            ? _selectedMicroserice!.readme.readme
+                .replaceAll(RegExp(r'\|\s+$', multiLine: true), "|")
+            : "",
+        imageBuilder: (uri, title, alt) =>
+            Image(image: CachedNetworkImageProvider(uri.toString())),
+      ),
+    );
+  }
+
+  buildContentActionBar() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            _selectedMicroserice != null
+                ? IconButton(
+                    onPressed: () {
+                      launch(_selectedMicroserice!.repo.url);
+                    },
+                    icon: SvgPicture.asset('assets/icons/octocat.svg'),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildNav(context) {
+    return Container(
+      width: 300,
+      child: ListView(
+        children: buildNavItems(context),
+      ),
+    );
+  }
+
+  buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.refresh),
+        onPressed: () {
+          load();
+        },
+      ),
+      IconButton(
+        icon: Icon(Icons.settings),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SettingsPage()),
+          );
+        },
+      ),
+    ];
+  }
+
+  buildNavItems(context) {
+    if (_microservices == null) return [];
+    var teams = _microservices!.map((it) => it.metadata.team).toSet().toList();
+
+    return teams.expand((team) {
+      return [
+        ListTile(
+          title: Text(
+            team,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        ..._microservices!
+            .where((it) => it.metadata.team == team)
             .map(
               (it) => ListTile(
-                title: Text(
-                  it.repo.name,
-                ),
+                title: Text(it.repo.name),
+                subtitle: Text(it.metadata.description),
                 onTap: () {
                   setState(() {
                     _selectedMicroserice = it;
@@ -124,7 +156,8 @@ class _ReadmesPageState extends State<ReadmesPage> {
                 },
               ),
             )
-            .toList()
-        : [];
+            .toList(),
+      ];
+    }).toList();
   }
 }
